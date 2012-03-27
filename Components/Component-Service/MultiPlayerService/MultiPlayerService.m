c59172e6d3bbec0344555d80fae865a52a87f5d2
kira <ouyongyong@163.com>
2012-03-27 11:08:07 +0800

    add core and components

---
+++ Components/Component-Service/MultiPlayerService/MultiPlayerService.m
@@ -0,0 +1,159 @@
+//
+//  MultiPlayerService.m
+//  Shuriken
+//
+//  Created by Orange on 12-2-13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "MultiPlayerService.h"
+
+@implementation MultiPlayerService
+@synthesize delegate = _delegate;
+@synthesize bluetoothService = _bluetoothService;
+
+- (void)dealloc
+{
+    [_bluetoothService release];
+    [super dealloc];
+}
+
+- (id)initWithMultiPlayerGameType:(NSInteger)aType
+{
+    self = [super init];
+    if (self) {
+        _multiGameType = aType;
+        switch (aType) {
+            case BLUETOOTH_GAME:
+                _bluetoothService = [[BlueToothService alloc] init];
+                _bluetoothService.delegate = self;
+                break;
+            case GAME_CENTER_GAME: {
+            }
+                break;
+            default:
+                break;
+        }
+
+    }
+    return self;
+}
+
+- (void)sendDataToAllPlayers:(NSData*)data
+{
+    switch (_multiGameType) {
+        case BLUETOOTH_GAME:
+            [_bluetoothService sendData:data];
+            break;
+        case GAME_CENTER_GAME: {
+            [[GameCenterService sharedInstance] sendData:data];
+        }
+        default:
+            break;
+    } 
+}
+
+- (void)findPlayers:(UIViewController*)superController
+{
+    switch (_multiGameType) {
+        case BLUETOOTH_GAME:
+            if (_bluetoothService == nil) {
+                _bluetoothService = [[BlueToothService alloc] init];
+            }
+            [_bluetoothService findDevice];
+            break;
+        case GAME_CENTER_GAME: {
+            [[GameCenterService sharedInstance] findMatchWithMinPlayers:2 maxPlayers:2 viewController:superController delegate:self];
+        }
+            break;
+        default:
+            break;
+    }
+    
+}
+
+- (void)quitMultiPlayersGame
+{
+    if (_bluetoothService && _multiGameType == BLUETOOTH_GAME) {
+        [_bluetoothService disconnectFromAllPeers];
+    } else {
+        [[GameCenterService sharedInstance] quitMatch];
+    }
+}
+
+#pragma mark - bluetooth service delegate
+- (void)receiveData: (NSData*) data fromPeer: (NSString*) peerID
+{
+    if (_delegate && [_delegate respondsToSelector:@selector(gameRecieveData:from:)]) {
+        [_delegate gameRecieveData:data from:peerID];
+    }
+}
+
+- (void)connectDeviceSuccessfully:(NSString*)peerId
+{
+    if (_delegate && [_delegate respondsToSelector:@selector(multiPlayerGamePrepared)]) {
+        [_delegate multiPlayerGamePrepared];
+    }
+}
+
+- (void)peerDisconnectFromSession:(NSString*)peerId;
+
+{
+    if (_delegate && [_delegate respondsToSelector:@selector(playerLeaveGame:)]) {
+        [_delegate playerLeaveGame:peerId];
+    }
+    
+}
+
+- (void)peerPickerCanceled
+{
+    if (_delegate && [_delegate respondsToSelector:@selector(gameCanceled)]) {
+        [_delegate gameCanceled];
+    }
+}
+
+#pragma mark - Game Center Delegate
+- (void)cancelFromGameCenter
+{
+    if (_delegate && [_delegate respondsToSelector:@selector(gameCanceled)]) {
+        [_delegate gameCanceled];
+    }
+}
+
+- (void)matchStarted
+{
+    if (_delegate && [_delegate respondsToSelector:@selector(multiPlayerGamePrepared)]) {
+        [_delegate multiPlayerGamePrepared];
+    }
+}
+
+- (void)quitGame
+{
+    if (_delegate && [_delegate respondsToSelector:@selector(gameCanceled)]) {
+        [_delegate gameCanceled];
+    }
+}
+
+- (void)playerLeaveGame:(NSString *)playerId
+{
+    if (_delegate && [_delegate respondsToSelector:@selector(playerLeaveGame:)]) {
+        [_delegate playerLeaveGame:playerId];
+    }
+}
+
+- (void)match:(GKMatch *)match didReceiveData:(NSData *)data 
+   fromPlayer:(NSString *)playerID
+{
+    if (_delegate && [_delegate respondsToSelector:@selector(gameRecieveData:from:)]) {
+        [_delegate gameRecieveData:data from:playerID];
+    }
+}
+
+- (void)inviteReceived
+{
+    if (_delegate && [_delegate respondsToSelector:@selector(multiPlayerGamePrepared)]) {
+        [_delegate multiPlayerGamePrepared];
+    }
+}
+
+@end
---
+++ Core/Core-Utils-Misc/Audio/AudioManager.h
@@ -0,0 +1,30 @@
+//
+//  AudioManager.h
+//  Shuriken
+//  这里需要AVFoundation.framework, AudioToolbox.framework
+//  Created by Orange on 12-3-6.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@class AVAudioPlayer;
+
+@interface AudioManager : NSObject {
+    AVAudioPlayer* _backgroundMusicPlayer;
+    NSMutableArray* _sounds;
+}
+@property (retain, nonatomic) AVAudioPlayer* backgroundMusicPlayer;
+@property (retain, nonatomic) NSMutableArray* sounds;
++ (AudioManager*)defaultManager;
+//播放短音效的方法。使用前必须使用Initsound的方法把需要的短音效注册好。soundNams音效名的array,建议用wav。
+- (void)initSounds:(NSArray*)soundNames;
+- (void)playSoundById:(NSInteger)aSoundIndex;
+- (void)vibrate;
+//下面是背景音乐播放的方法，目前功能暂时已注释掉，无法使用
+- (void)setBackGroundMusicWithName:(NSString*)aMusicName;
+- (void)backgroundMusicStart;
+- (void)backgroundMusicPause;
+- (void)backgroundMusicContinue;
+- (void)backgroundMusicStop;
+
+@end
