384e23d911f375c61fa16c95ba0258840e1e0bf6
Benson Peng <penglingzhe@gmail.com>
2012-04-06 00:02:36 +0800

    add facebook SDK

---
+++ Components/Component-Service/SNS/Service/FacebookSNSService.h
@@ -0,0 +1,13 @@
+//
+//  FacebookSNSService.h
+//  Draw
+//
+//  Created by  on 12-4-5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FacebookSNSService : NSObject
+
+@end
---
+++ Components/Component-Service/SNS/Service/FacebookSNSService.m
@@ -0,0 +1,13 @@
+//
+//  FacebookSNSService.m
+//  Draw
+//
+//  Created by  on 12-4-5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "FacebookSNSService.h"
+
+@implementation FacebookSNSService
+
+@end
