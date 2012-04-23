60a9d7cce198c3a510e978c641911f4b66141c06
Benson Peng <penglingzhe@gmail.com>
2012-04-23 12:16:34 +0800

    add store kit utils

---
+++ Core/Core-Utils-Misc/StoreKit/StoreKitUtils.m
@@ -0,0 +1,13 @@
+//
+//  StoreKitUtils.m
+//  Draw
+//
+//  Created by  on 12-4-23.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "StoreKitUtils.h"
+
+@implementation StoreKitUtils
+
+@end
