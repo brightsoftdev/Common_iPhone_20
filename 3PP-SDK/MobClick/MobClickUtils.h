07434e5590165c057ec506084e632e58ea0b3978
Benson Peng <penglingzhe@gmail.com>
2012-04-28 21:47:42 +0800

    add mobclick utils

---
+++ 3PP-SDK/MobClick/MobClickUtils.h
@@ -0,0 +1,13 @@
+//
+//  MobClickUtils.h
+//  Draw
+//
+//  Created by  on 12-4-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MobClickUtils : NSObject
+
+@end
---
+++ 3PP-SDK/MobClick/MobClickUtils.m
@@ -0,0 +1,13 @@
+//
+//  MobClickUtils.m
+//  Draw
+//
+//  Created by  on 12-4-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "MobClickUtils.h"
+
+@implementation MobClickUtils
+
+@end
